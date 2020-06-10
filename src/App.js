e67a7dbddab0917e4a30b4376617e774a48100e2
wmc2tokyo <wmcooper2@yahoo.com>
2020-06-10 20:59:20 +0900

    Added some notes to the home page as a reminder.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>Make Favicon icon</li>
+          <li>Install react-router</li>
+          <li>Install helmet</li>
+          <li>Install mongoDB</li>
+          <li>Wireframe app</li>
+          <li>Flesh out the data structure in the DB</li>
+          <li>Figure out overlay in react</li>
+          <li>Make API endpoints and test them (CRUD)</li>
+          <li>Test out fetch() in the app</li>
+        </ul>
       </header>
     </div>
   );
